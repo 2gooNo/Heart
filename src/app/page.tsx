755e9034961b5591a197c6749bd5f4c23682cc6b
2gooNo <143684446+2gooNo@users.noreply.github.com>
2024-06-29 15:09:28 +0800

    heart hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
         <p>Xairiga udsand uuchlaarai hh</p>
         <p>Xairn bolgn gej helsen shvde </p>
       </div>
-      <img src="Heart.png" className="w-full h-100vw" />
+      <img
+        src="https://hips.hearstapps.com/hmg-prod/images/heart-emoji-meanings-65c3b01e11012.jpg?crop=0.114xw:0.416xh;0.0170xw,0.0994xh&resize=980:*"
+        className="w-[700px] h-100vw"
+      />
     </div>
   );
 }
